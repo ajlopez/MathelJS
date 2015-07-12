c236ff13012d701e057678e0ad333563c5b366e5
ajlopez <webmaster@ajlopez.com>
2015-07-12 12:05:08 -0300

    Undefined values in matrix, as zero; empty matrix

--- test/matrices.js
+++ test/matrices.js
@@ -35,3 +35,26 @@ exports['create matrix with values'] = function (test) {
     test.done();
 };
 
+exports['create matrix without options'] = function (test) {
+    var matrix = matrices.createMatrix();
+    
+    test.ok(matrix);
+    test.equal(typeof matrix, 'object');
+    test.equal(matrix.nrows(), 0);
+    test.equal(matrix.ncolumns(), 0);
+    
+    test.done();
+};
+
+exports['get undefined values as zero'] = function (test) {
+    var matrix = matrices.createMatrix({ values: [ [ 1, 2, 3], [ 4, 5, 6 ] ] });
+    
+    test.strictEqual(matrix.element(-1, 1), 0);
+    test.strictEqual(matrix.element(-2, -3), 0);
+    test.strictEqual(matrix.element(1, -2), 0);
+    test.strictEqual(matrix.element(4, 1), 0);
+    test.strictEqual(matrix.element(1, 10), 0);
+    test.strictEqual(matrix.element(20, 30), 0);
+    
+    test.done();
+};
